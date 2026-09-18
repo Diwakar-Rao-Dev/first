50d26e43565de3d9bc7edf06b54a98176abf75f6
Diwakar-Rao-Dev <rajputdiwakar001@gmail.com>
2026-09-18 14:58:26 +0530

    done

--- JSFolder/index.js
+++ JSFolder/index.js
@@ -1,5 +1,14 @@
-let a = "hii"
-a = "hlo"
-console.log(a);
+// let a = "hii"
+// a = "hlo"
+// console.log(a);
+
+
+let student = [
+     {name:"Diwakar", homecity:"ramkola kushinagar", age:22},
+     {name:"ashish", homecity:"ramkola kushinagar", age:22},
+     {name:"aman", homecity:"gorkhapur", age:23}
+];
+console.table(student);
+
 
 
